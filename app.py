355f22cfa0a4ba4175b9519c1e23111de95f9daa
Ahmad Naufal <ahmadnaufals123@gmail.com>
2023-08-02 10:38:34 +0700

    first github project

---
+++ app.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("Masukkan alas segi3: "))
+    t = int(input("Masukkan tinggi segi3:"))
+    luas = a * t / 2
+    print("Luas segi3 adalah: ", luas)
+
+luas_segitiga()
